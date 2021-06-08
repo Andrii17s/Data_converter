d3d1932eeb153894463748fd57b9e43996d4f44b
Andrii17s <75915283+Andrii17s@users.noreply.github.com>
2021-06-08 12:57:53 +0300

    Update scoring_rule.py
    Rule 3

--- business_register/converter/company_converters/scoring_rule.py
+++ business_register/converter/company_converters/scoring_rule.py
@@ -1,20 +1,36 @@
 from abc import ABC, abstractmethod
+from business_register.models.declaration_models import (Declaration,
+                                                         Property,
+                                                         PropertyRight,
+                                                         LuxuryItem,
+                                                         LuxuryItemRight,
+                                                         Vehicle,
+                                                         VehicleRight,
+                                                         Securities,
+                                                         SecuritiesRight,
+                                                         Income,
+                                                         Money,
+                                                         )
 
 
 # docstring
 
 class BaseScoringRule(ABC):
 
-    def __init__(self):
-        pass
+    def __init__(self, pep_id):
+        self.declarations_id = []
+        for declaration in Declaration.objects.raw('SELECT * from business_register_declaration WHERE id=pep_id'):
+            self.declarations_id.append(declaration.id)
 
     @abstractmethod
     def calculate_wage(self):
         pass
 
 class IsRealEstateWithoutValue(BaseScoringRule): #3 rule
-    def __init__(self):
-        pass
-
     def calculate_wage(self):
-        pass
+        for declaration_id in self.declarations_id:
+            for property in Property.objects.raw('SELECT * from business_register_property WHERE declaration_id=declaration_id'):
+                print(property.valuation)
+
+x = IsRealEstateWithoutValue(1)
+x.calculate_wage()
