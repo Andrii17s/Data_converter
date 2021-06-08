02672daedc898c74a66ec00fdeac8c4b1f8d843c
Andrii17s <75915283+Andrii17s@users.noreply.github.com>
2021-06-08 14:19:12 +0300

    Update scoring_rule.py
    Rule 3

--- business_register/converter/company_converters/scoring_rule.py
+++ business_register/converter/company_converters/scoring_rule.py
@@ -19,18 +19,28 @@ class BaseScoringRule(ABC):
 
     def __init__(self, pep_id):
         self.declarations_id = []
-        for declaration in Declaration.objects.raw('SELECT * from business_register_declaration WHERE id=pep_id'):
+        for declaration in Declaration.objects.raw('SELECT * from business_register_declaration WHERE id=%s', [pep_id]):
             self.declarations_id.append(declaration.id)
 
     @abstractmethod
     def calculate_wage(self):
         pass
 
-class IsRealEstateWithoutValue(BaseScoringRule): #3 rule
+
+class IsRealEstateWithoutValue(BaseScoringRule):  # 3 rule
+
     def calculate_wage(self):
         for declaration_id in self.declarations_id:
-            for property in Property.objects.raw('SELECT * from business_register_property WHERE declaration_id=declaration_id'):
-                print(property.valuation)
+            for pep_property in Property.objects.raw('SELECT * from business_register_property WHERE declaration_id=%s',
+                                                     [declaration_id]):
+                print(pep_property.valuation)
+                if (pep_property.valuation is None) and (pep_property.type == 2) and (
+                        str(pep_property.created_at.date()) > '2014-12-31'):
+                    return 0.4
+                else:
+                    pass
+        return 0
+
 
 x = IsRealEstateWithoutValue(1)
-x.calculate_wage()
+print(x.calculate_wage())
