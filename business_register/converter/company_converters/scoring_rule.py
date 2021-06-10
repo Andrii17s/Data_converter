8399cf10ef68221f2fdf4d80dd072219edd3111e
Andrii17s <75915283+Andrii17s@users.noreply.github.com>
2021-06-10 17:13:39 +0300

    Update scoring_rule

--- business_register/converter/company_converters/scoring_rule.py
+++ business_register/converter/company_converters/scoring_rule.py
@@ -18,30 +18,36 @@ from location_register.models.ratu_models import (RatuCity, )
 # docstring
 
 class BaseScoringRule(ABC):
-
-    def __init__(self, pep_id): #pep object, not id
-        self.pep_id = pep_id
-        self.family_declarations_id = []
-        self.pep_declarations_id = []
-        category = 'family'
-        for family_member in RelatedPersonsLink.objects.raw('SELECT * FROM business_register_relatedpersonslink WHERE '
-                                                            'from_person_id=%s AND category=%s', [pep_id, category]):
-            print(family_member.to_person_id)
-            for declaration in Declaration.objects.raw('SELECT * FROM business_register_declaration WHERE id=%s',
-                                                       [family_member.to_person_id]):
-                self.family_declarations_id.append(declaration.id)
-
-        for declaration in Declaration.objects.raw('SELECT * FROM business_register_declaration WHERE id=%s',
-                                                   [pep_id]):
-            self.pep_declarations_id.append(declaration.id)
-        self.family_declarations_id.append(self.pep_declarations_id)
-
+    def __init__(self, pep):
+        self.pep = pep
     @abstractmethod
-    def calculate_wage(self):
+    def calculate_weight(self):
         pass
 
 
-class IsRealEstateWithoutValue(BaseScoringRule):  # 3_1 rule
+class IsRealEstateWithoutValue(BaseScoringRule):
+    """
+    Rule 3.1 - PEP03_home
+    weight - 0.4
+    There is no information on the value of the realestate owned by PEP or
+    family members since 2015
+    """
+    def calculate_weight(self):
+        family_ids = self.pep.related_persons.filter(
+            related_persons__category=RelatedPersonsLink.FAMILY,
+        ).values_list('id', flat=True)
+        family_ids.append(self.pep.id)
+        have_weight = Property.objects.filter(
+            declaration__pep_id__in=family_ids,
+            valuation__isnull=True,
+            type=Property.SUMMER_HOUSE,
+            created_at__year__gte=2015,
+        ).exests()
+        if have_weight:
+            return 0.4
+        return 0
+
+class IsRealEstateWithoutValuee(BaseScoringRule):  # 3_1 rule
 
     def calculate_wage(self):
         for declaration_id in self.family_declarations_id:
@@ -56,7 +62,12 @@ class IsRealEstateWithoutValue(BaseScoringRule):  # 3_1 rule
 
 
 class IsLandWithoutValue(BaseScoringRule):  # 3_2 rule
-
+    """
+    Rule 3.2 - PEP03_land
+    weight - 0.1
+    There is no information on the value of the land owned by PEP or
+    family members since 2015
+    """
     def calculate_wage(self):
         for declaration_id in self.family_declarations_id:
             for pep_property in Property.objects.raw('SELECT * from business_register_property WHERE declaration_id=%s',
@@ -70,7 +81,12 @@ class IsLandWithoutValue(BaseScoringRule):  # 3_2 rule
 
 
 class IsAutoWithoutValue(BaseScoringRule):  # 3_3 rule
-
+    """
+    Rule 3.3 - PEP03_car
+    weight - 0.4
+    There is no information on the value of the vehicle owned by PEP or
+    family members since 2015
+    """
     def calculate_wage(self):
         for declaration_id in self.family_declarations_id:
             for pep_car in Vehicle.objects.raw('SELECT * from business_register_vehicle WHERE declaration_id=%s',
@@ -84,7 +100,12 @@ class IsAutoWithoutValue(BaseScoringRule):  # 3_3 rule
 
 
 class LiveNowhere(BaseScoringRule):  # 4_1 rule
-
+    """
+    Rule 4.1 - PEP04_adr
+    weight - 0.7
+    There is no information on the value of the realestate owned by PEP or
+    family members since 2015
+    """
     def calculate_wage(self):
         live_nowhere = True
         for declaration_id in self.pep_declarations_id:
@@ -105,7 +126,12 @@ class LiveNowhere(BaseScoringRule):  # 4_1 rule
 
 
 class LiveNowhereRegion(BaseScoringRule):  # 4_2 rule
-
+    """
+    Rule 4.2 - PEP04_reg
+    weight - 0.1
+    There is no information on the value of the realestate owned by PEP or
+    family members since 2015
+    """
     def calculate_wage(self):
         live_nowhere = True
         for declaration_id in self.pep_declarations_id:
@@ -131,7 +157,31 @@ class LiveNowhereRegion(BaseScoringRule):  # 4_2 rule
 
 
 class LuxuaryCars(BaseScoringRule):  # 18 rule -
+    """
+    Rule 18 - PEP18
+    weight - 0.4
+    There is no information on the value of the realestate owned by PEP or
+    family members since 2015
+    """
+    def calculate_wage(self):
+        for declaration_id in self.pep_declarations_id:
+            for pep_car in Vehicle.objects.raw('SELECT * from business_register_vehicle WHERE declaration_id=%s',
+                                               [declaration_id]):
+                print(pep_car.valuation)
+                if (pep_car.valuation is None) and (str(pep_car.created_at.date()) > '2014-12-31'):
+                    return 0.4
+                else:
+                    pass
+        return 0
 
+
+class CarsCount(BaseScoringRule):  # 19 rule -
+    """
+    Rule 19 - PEP19
+    weight - 0.4
+    There is no information on the value of the realestate owned by PEP or
+    family members since 2015
+    """
     def calculate_wage(self):
         for declaration_id in self.pep_declarations_id:
             for pep_car in Vehicle.objects.raw('SELECT * from business_register_vehicle WHERE declaration_id=%s',
@@ -145,7 +195,12 @@ class LuxuaryCars(BaseScoringRule):  # 18 rule -
 
 
 class CashTotalAmount(BaseScoringRule):  # 20 rule -
-
+    """
+    Rule 20 - PEP20
+    weight - 0.8
+    There is no information on the value of the realestate owned by PEP or
+    family members since 2015
+    """
     def calculate_wage(self):
         for declaration_id in self.family_declarations_id:
             for pep_property in Money.objects.raw('SELECT * from business_register_money WHERE declaration_id=%s',
@@ -158,5 +213,5 @@ class CashTotalAmount(BaseScoringRule):  # 20 rule -
         return 0
 
 
-x = LuxuaryCars(1)
-print(x.calculate_wage())
+x = IsRealEstateWithoutValue(Property.objects.raw('SELECT * from business_register_pep WHERE id=1'))
+print(x.calculate_weight())
