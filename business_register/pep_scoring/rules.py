c057ffc09778c6495d09a7777407b31e3f93503d
Andrii17s <75915283+Andrii17s@users.noreply.github.com>
2021-06-27 17:59:52 +0300

    Update rules.py
    Rule 22

--- business_register/pep_scoring/rules.py
+++ business_register/pep_scoring/rules.py
@@ -104,3 +104,59 @@ class IsAutoWithoutValue(BaseScoringRule):
             }
             return weight, data
         return 0, {}
+
+
+class IsMultiplyingMoney(BaseScoringRule):
+    """
+    Rule 22 - PEP22
+    weight - 0.8
+    Hard cash declared in the very first electronic asset declaration available in
+    the system  exceeds in 5 or more times income declared for the corresponding year
+    """
+
+    def calculate_weight(self):
+        declarations = Declaration.objects.filter(
+            pep_id=self.pep.id,
+        ).values('id', 'year')[::1]
+        declaration_ids = {}
+
+        for declaration in declarations:
+            year = declaration['year']
+            if not declaration_ids.__contains__(year):
+                declaration_ids[declaration['year']] = list()
+                declaration_ids[declaration['year']].extend([declaration['id']])
+            elif not declaration['id'] in declaration_ids[year]:
+                declaration_ids[year].extend([declaration['id']])
+        id_sort_by_year = sorted(declaration_ids.items(), key=lambda x: x[0])
+        assets = Money.objects.filter(
+            declaration_id=id_sort_by_year[0][1][0],
+        ).values_list('amount', 'currency')[::1]
+        incomes = Income.objects.filter(
+            declaration_id=id_sort_by_year[0][1][0],
+        ).values_list('amount', flat=True)[::1]
+        total_income = 0
+        for income in incomes:
+            total_income += income
+        assets_UAH = 0
+        assets_USD = 0
+        assets_EUR = 0
+        assets_GBP = 0
+        for asset in assets:
+            if asset[1] == 'UAH':
+                assets_UAH += asset[0]
+            elif asset[1] == 'USD':
+                assets_USD += asset[0]
+            elif asset[1] == 'EUR':
+                assets_EUR += asset[0]
+            else:
+                assets_GBP += asset[0]
+        total_assets = assets_USD * 27 + assets_EUR * 32.7 + assets_GBP * 38 + assets_UAH #!
+        if (total_assets / total_income) > 5:
+            weight = 0.8
+            data = {
+                "total_assets": total_assets,
+                "total_income": total_income,
+                "year": id_sort_by_year[0][0]
+            }
+            return weight, data
+        return 0, {}
